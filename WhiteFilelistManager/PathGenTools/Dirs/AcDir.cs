b08173ca93930387d5b9561dfc4f95e517cb9d0f
Surihix <neweramysterymailer@gmail.com>
2025-08-22 20:40:55 +0530

    fixed ac directory filetype string for 13-1

--- WhiteFilelistManager/PathGenTools/Dirs/AcDir.cs
+++ WhiteFilelistManager/PathGenTools/Dirs/AcDir.cs
@@ -121,6 +121,7 @@ namespace WhiteFilelistManager.PathGenTools.Dirs
                             fileCode = finalComputedBits.BinaryToUInt(0, 32).ToString();
 
                             GenerationVariables.FileCode = fileCode;
+                            GenerationVariables.FileTypeID = "13";
                             break;
 
                         case "DataSet":
